5e38b41457731c435f834278a2db6788f3696bc9
Lee Yen Ter <leeyenter@gmail.com>
2022-06-21 01:13:31 +0800

    Fully migrate to React

---
+++ frontend/src/Context.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export interface UserInterface {
+  id: number;
+  name: string;
+  pic: string;
+}
+
+export interface CommentInterface {
+  id: number;
+  comment: string;
+  user: number;
+  created: Date;
+  createdPretty: string;
+  upvotes: any[];
+}
+
+export const defaultUser: UserInterface = { id: 0, name: "", pic: "" };
+const emptyUsers: UserInterface[] = [];
+const emptyComments: CommentInterface[] = [];
+
+export const Context = React.createContext({
+  user: defaultUser,
+  users: emptyUsers,
+  changeUser: () => {},
+  comments: emptyComments,
+  fetchComments: () => {},
+  addUpvote: (id: number) => {},
+  removeUpvote: (id: number) => {},
+});
